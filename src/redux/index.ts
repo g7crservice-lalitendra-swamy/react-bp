297255eed209a11bbdecb974ee1c9dd22ed195a7
Lalitendra Swamy <lalitendra.swamy@g7cr.com>
2025-03-03 18:34:45 +0530

    Updates

--- src/redux/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import { getPostsSliceData } from './features/bestPractices/GetPosts';
-
-const rootReducer = combineReducers({
-  getPostsData:getPostsSliceData
-});
-
-export default rootReducer;
--- src/shared/api-endpoints.ts
+++
@@ -1,3 +0,0 @@
-export const API_ENDPOINTS={
-    GET_POSTS:'https://jsonplaceholder.typicode.com/posts'
-}
